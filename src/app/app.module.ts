2018fb3478ae363ab54a3382f557cd615404bb48
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-20 07:07:09 +0530

    addFaculty

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AddFacultyComponent } from './add-faculty/add-faculty.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AddFacultyComponent
   ],
   imports: [
     BrowserModule,
